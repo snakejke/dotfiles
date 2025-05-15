18145d73acdfec3858a45962279fa23b63cc988e
Artem Bliznetsov <snakejke@proton.me>
2025-05-15 15:31:58 +0300

    changes

--- ansible_main/bootstrap.sh
+++ ansible_main/bootstrap.sh
@@ -4,27 +4,27 @@ set -e
 echo "👉 Проверка необходимых пакетов: git, rsync, ansible..."
 
 # Устанавливаем нужные пакеты, если не установлены
-for pkg in git rsync ansible; do
-  if ! command -v "$pkg" > /dev/null 2>&1; then
-    echo "📦 Устанавливаю $pkg..."
-    sudo xbps-install -Sy "$pkg"
-  else
-    echo "✅ $pkg уже установлен"
-  fi
-done
+# for pkg in git rsync ansible; do
+#   if ! command -v "$pkg" > /dev/null 2>&1; then
+#     echo "📦 Устанавливаю $pkg..."
+#     sudo xbps-install -Sy "$pkg"
+#   else
+#     echo "✅ $pkg уже установлен"
+#   fi
+# done
 
 # Клонируем твой репозиторий, если нужно
-REPO_URL="https://github.com/yourname/dotfiles.git"
-CLONE_DIR="$HOME/.local/ansible_main"
+REPO_URL="https://github.com/snakejke/dotfiles"
+CLONE_DIR="$HOME/target_home/.local/dotfiles/"
 
-if [ ! -d "$CLONE_DIR" ]; then
-  echo "📥 Клонируем репозиторий $REPO_URL в $CLONE_DIR..."
+# if [ ! -d "$CLONE_DIR" ]; then
+#   echo "📥 Клонируем репозиторий $REPO_URL в $CLONE_DIR..."
   git clone "$REPO_URL" "$CLONE_DIR"
-else
-  echo "🔄 Репозиторий уже существует: $CLONE_DIR"
-fi
+# else
+#   echo "🔄 Репозиторий уже существует: $CLONE_DIR"
+# fi
 
-cd "$CLONE_DIR"
+cd "$CLONE_DIR/ansible_main"
 
 # Запускаем установку ansible
 echo "🚀 Запуск ansible-playbook install.yml..."
